36b35fda6dbb14e701bf3e9a3162f0b0cee15fc7
ivalencius <valencig@bc.edu>
2025-05-28 15:04:52 -0400

    Got rid of stupid config file

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+Various constants that are useful for all files
+"""
+import os
+import yaml
+
+# Quality of life imports
+#from rich import print
+
+### Change these paths for your own use
+CWD = '/home/valencig/GPLLJ/' # change this
+SCRATCH_DIR = '/scratch/valencig/GPLLJ-Scratch/'
+###
+
+os.chdir(CWD)
+DATA_DIR = os.path.join(CWD, 'data/')
